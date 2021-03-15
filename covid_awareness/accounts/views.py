03473f8ce292ed8e4606d1709da8e135ff62ae5d
SothSambath <sambathsothlimalove@gmail.com>
2021-03-15 15:58:59 -0700

    add static file

--- covid_awareness/accounts/views.py
+++ covid_awareness/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 
@@ -10,13 +11,22 @@ def register(request):
         password2 = request.POST['password2']
 
         if password1 == password2:
-            user = User.objects.create_user(
-                username=username,
-                email=email,
-                password=password1
-            )
-            user.save()
-            return redirect('hospital:index')
+            if User.objects.filter(username=username).exists():
+                messages.add_message(request, messages.INFO, 'Username taken')
+                return redirect('accounts:register')
+            elif User.objects.filter(email=email).exists():
+                messages.add_message(request, messages.INFO, 'Email taken')
+                return redirect('accounts:register')
+            else: 
+                user = User.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=password1
+                )
+                user.save()
+                return redirect('hospital:index')
+        else:
+            messages.add_message(request, messages.INFO, "Password doesn't match")
     return render(request, 'accounts/register.html')
 
 
